ae52814e9f0b21f08b9cc929e90e28a58c986d23
Azeem U Rehman <greatthedevolper@gmail.com>
2024-07-11 09:17:42 +0500

    updated 2

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,7 @@ import TableView from '@/components/VueTable.vue'
 </script>
 <template>
   <div class="container-full px-4 my-4 max-w-full">
-    <h1>This is an about page</h1>
+    <h1>sure This is an about page</h1>
     <TableView />
   </div>
 </template>
