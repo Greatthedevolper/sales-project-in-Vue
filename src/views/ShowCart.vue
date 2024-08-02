b0bd342b4fcb2affe4c4bbbfb8e6f04f35e73ced
Azeem U Rehman <greatthedevolper@gmail.com>
2024-08-03 00:51:08 +0500

    updated cart design

--- src/views/ShowCart.vue
+++ src/views/ShowCart.vue
@@ -13,33 +13,62 @@ function decrement(productId: number) {
 </script>
 
 <template>
-  <div>
-    <ul>
-      <li
-        v-for="(product, index) in cartStore.addedCartItems"
-        :key="index"
-        class="text-[var(--hover-color)] flex items-center gap-2 flex-wrap"
-      >
-        <img :src="product.thumbnail" alt="Product Image" class="w-[120px] h-[120px]" />
-        <span>{{ product.title }}</span>
-        <span>{{ product.price }}$</span>
-        <div class="inline-flex items-center border border-[var(--hover-color)] rounded w-[100px]">
-          <span
-            @click="increment(product.id)"
-            class="w-1/3 border border-[var(--hover-color)] inline-flex items-center justify-center text-[20px]"
-            >+</span
-          >
-          <span
-            class="w-1/3 border border-[var(--hover-color)] inline-flex items-center justify-center text-[20px]"
-            >{{ product.quantity }}</span
-          >
-          <span
-            @click="decrement(product.id)"
-            class="w-1/3 border border-[var(--hover-color)] inline-flex items-center justify-center text-[20px]"
-            >-</span
-          >
-        </div>
+  <div class="p-3">
+    <h1 class="main-heading">Product in your Cart</h1>
+    <ul class="">
+      <li class="grid grid-cols-5 text-[var(--hover-color)] my-3">
+        <span class="text-center inline-block">Image</span>
+        <span class="text-center inline-block">Name</span>
+        <span class="text-center inline-block">Price</span>
+        <span class="text-center inline-block">Add More</span>
+        <span class="text-center inline-block">Payment</span>
       </li>
+      <teemplate v-if="cartStore.addedCartItems.length > 0">
+        <li
+          v-for="(product, index) in cartStore.addedCartItems"
+          :key="index"
+          class="text-[var(--hover-color)] grid grid-cols-5 items-center mb-4"
+        >
+          <template v-if="product.id">
+            <div class="w-full flex items-center justify-center">
+              <div
+                class="w-[80px] h-[80px] border border-[var(--hover-color)] rounded-full p-1 mx-uato"
+              >
+                <img :src="product.thumbnail" alt="Product Image" class="w-full h-full" />
+              </div>
+            </div>
+            <router-link
+              :to="`/single-product?id=${product.id}`"
+              class="text-center inline-block"
+              >{{ product.title }}</router-link
+            >
+            <span class="text-center inline-block">{{ product.price }}$</span>
+            <div class="flex items-center justify-center">
+              <div
+                class="inline-flex items-center border border-[var(--hover-color)] rounded w-[100px]"
+              >
+                <span
+                  role="button"
+                  @click="increment(product.id)"
+                  class="inline-flex items-center justify-center text-[20px] w-[33px] h-[33px]"
+                  >+</span
+                >
+                <span
+                  class="border border-x-[var(--hover-color)] border-y-0 inline-flex items-center justify-center text-[20px] w-[33px] h-[33px]"
+                  >{{ product.quantity }}</span
+                >
+                <span
+                  role="button"
+                  @click="decrement(product.id)"
+                  class="inline-flex items-center justify-center text-[20px] w-[33px] h-[33px]"
+                  >-</span
+                >
+              </div>
+            </div>
+            <span class="text-center inline-block">{{ product.price * product.quantity }}$</span>
+          </template>
+        </li>
+      </teemplate>
     </ul>
   </div>
 </template>
